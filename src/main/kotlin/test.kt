d1903f887e0534b9bb80cacd1a11c97f6bacf7f0
benpospo@gmail.com <93595333+taraxsmallboy@users.noreply.github.com>
2023-05-28 23:45:05 +0200

    changes

--- src/main/kotlin/test.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-    val input = readLine() ?: return // Read user input
-    val command = "print"
-
-    if (input.startsWith(command)) {
-        val quotedPart = input.substringAfter("\"").substringBeforeLast("\"")
-        println(quotedPart)
-    }
-}
