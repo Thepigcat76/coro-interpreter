736ddf02c5ceb4991d9cbd5426ac4516ae776e93
benpospo@gmail.com <93595333+taraxsmallboy@users.noreply.github.com>
2023-05-29 17:48:44 +0200

    the lexer

---
+++ src/main/kotlin/interpreter/Lexer.kt
@@ -0,0 +1,23 @@
+package interpreter
+
+import java.io.BufferedReader
+import java.io.FileReader
+
+class Lexer {
+    private var currentFile = "example.txt"
+    private val fileReader = FileReader(currentFile)
+    private val bufferedReader = BufferedReader(fileReader)
+    private val rawLines = bufferedReader.readLines()
+
+    init {
+        for (rawLine in rawLines) {
+            val splitLine = rawLine.split(Regex(" "))
+            for (line in splitLine) {
+                    println(line)
+                }
+            }
+        }
+    }
+fun main() {
+    Lexer()
+}
